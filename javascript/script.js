6282db8e8323087ba0543d8ae2c8bc043c33e97b
micaelsilvasouza <108833081+micaelsilvasouza@users.noreply.github.com>
2022-09-05 19:23:55 -0300

    endereço para entrega

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,32 @@
 function TrocarPagina(){
     let link = document.getElementById("link")
+    let recebimento = document.getElementsByName("retirada")
     let pagameto = document.getElementsByName("pagamento")
-    if(pagameto[0].checked){
-        link.setAttribute("href","pix.html")
+    if(recebimento[1].checked){
+        for(let i = 0; i < 4 ; i++){
+            let end = document.getElementsByName("end")[i]
+            if(end.value.length <= 0){
+                return alert("Preencha todo os campos corretante")
+            }
+        }
+        if(pagameto[0].checked){
+            link.setAttribute("href","pix.html")
+        }else{
+            link.setAttribute("href", "compra-finalizada.html")
+        }
     }else{
+        if(pagameto[0].checked){
+        link.setAttribute("href","pix.html")
+        }else{
         link.setAttribute("href", "compra-finalizada.html")
+        }
+    }
+}
+function AparecerEnder(fator){
+    let div = document.getElementById("endereco")
+    if(fator){
+        div.style.display = "block"
+    }else{
+        div.style.display = "none"
     }
 }
